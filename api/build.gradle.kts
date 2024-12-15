8d5eae6603f49f9bf1e6d21a90c3d728f98da9ba
bzp99 <bpeter@edu.bme.hu>
2024-12-15 20:12:24 +0100

    feat(api): add logging configuration

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,7 +7,9 @@ dependencies {
     implementation(libs.bundles.ktor.server)
     implementation(libs.ktor.server.html.builder)
     implementation(libs.slf4j.api)
-    runtimeOnly(libs.penna.core)
+
+    runtimeOnly(libs.bundles.penna)
+
     testImplementation(libs.ktor.server.test.host)
 }
 
