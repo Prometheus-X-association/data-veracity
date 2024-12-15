189e7e778d6fc153087b5dcc79e295afde5f6716
bzp99 <bpeter@edu.bme.hu>
2024-12-15 20:07:54 +0100

    feat(api): add call logging

--- api/src/main/kotlin/hu/bme/mit/ftsrg/dva/api/Application.kt
+++ api/src/main/kotlin/hu/bme/mit/ftsrg/dva/api/Application.kt
@@ -1,11 +1,28 @@
 package hu.bme.mit.ftsrg.dva.api
 
 import hu.bme.mit.ftsrg.dva.api.route.docRoutes
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.request.*
+import org.slf4j.event.Level
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
+  install(CallLogging) {
+    level = Level.DEBUG
+    format { call ->
+      val status: HttpStatusCode? = call.response.status()
+      val method = call.request.httpMethod.value
+      val path: String = call.request.path()
+      val time: Long = call.processingTimeMillis()
+
+      "$method $path -> $status in $time ms"
+    }
+  }
+
+
   docRoutes()
 }
