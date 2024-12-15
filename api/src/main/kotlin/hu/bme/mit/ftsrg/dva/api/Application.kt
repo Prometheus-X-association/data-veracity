510370f19d66fbdaaf6c8d335b16176cecc4e3f6
bp99 <bertalan.peter@bp99.eu>
2024-12-15 18:59:15 +0100

    refactor: extract ktor routes to separate files

--- api/src/main/kotlin/hu/bme/mit/ftsrg/dva/api/Application.kt
+++ api/src/main/kotlin/hu/bme/mit/ftsrg/dva/api/Application.kt
@@ -1,47 +1,11 @@
 package hu.bme.mit.ftsrg.dva.api
 
-import io.ktor.http.*
+import hu.bme.mit.ftsrg.dva.api.route.docRoutes
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
-import io.ktor.server.html.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.routing.*
-import kotlinx.html.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
-  routing {
-    swaggerRoute()
-    rootRoute()
-  }
-}
-
-fun Route.swaggerRoute() {
-  swaggerUI("swagger", swaggerFile = "spec/openapi.yaml")
-}
-
-fun Route.rootRoute() {
-  get("/") {
-    val name = "DVA"
-    call.respondHtml(HttpStatusCode.OK) {
-      head {
-        title {
-          +name
-        }
-      }
-      body {
-        h1 {
-          +"Welcome to the $name web UI"
-        }
-        p {
-          +"Click "
-          a(href = "/swagger") {
-            +"here"
-          }
-          +" to see the API documentation and test the service."
-        }
-      }
-    }
-  }
+  docRoutes()
 }
