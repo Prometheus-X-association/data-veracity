e063c5f6b0faf4b94a1d7ea8b8c0b49a6ff2ce70
bzp99 <bpeter@edu.bme.hu>
2024-12-15 19:48:16 +0100

    test(api): update unit tests

---
+++ api/src/test/kotlin/hu/bme/mit/ftsrg/dva/api/route/DocRoutesTest.kt
@@ -0,0 +1,38 @@
+package hu.bme.mit.ftsrg.dva.api.route
+
+import hu.bme.mit.ftsrg.dva.api.module
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class DocRoutesTest {
+
+  @Test
+  fun `should return HTML page when root route is requested`() = testApplication {
+    application {
+      module()
+    }
+    client.get("/").apply {
+      assertEquals(HttpStatusCode.OK, status)
+      assertTrue {
+        headers["Content-Type"]?.contains(ContentType.Text.Html.toString()) ?: false
+      }
+    }
+  }
+
+  @Test
+  fun `should return swagger documentation page when slash swagger is requested`() = testApplication {
+    application {
+      module()
+    }
+    client.get("/swagger").apply {
+      assertEquals(HttpStatusCode.OK, status)
+      assertTrue {
+        headers["Content-Type"]?.contains(ContentType.Text.Html.toString()) ?: false
+      }
+    }
+  }
+}
