a7e294fba472b5dd6edd629b3ab146424242dd98
bzp99 <bpeter@edu.bme.hu>
2024-12-15 18:59:15 +0100

    refactor(api): extract ktor routes

---
+++ api/src/main/kotlin/hu/bme/mit/ftsrg/dva/api/route/DocRoutes.kt
@@ -0,0 +1,44 @@
+package hu.bme.mit.ftsrg.dva.api.route
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.routing.*
+import kotlinx.html.*
+
+fun Application.docRoutes() {
+  routing {
+    rootRoute()
+    swaggerRoute()
+  }
+}
+
+fun Route.swaggerRoute() {
+  swaggerUI("swagger", swaggerFile = "spec/openapi.yaml")
+}
+
+fun Route.rootRoute() {
+  get("/") {
+    val name = "DVA"
+    call.respondHtml(HttpStatusCode.OK) {
+      head {
+        title {
+          +name
+        }
+      }
+      body {
+        h1 {
+          +"Welcome to the $name web UI"
+        }
+        p {
+          +"Click "
+          a(href = "/swagger") {
+            +"here"
+          }
+          +" to see the API documentation and test the service."
+        }
+      }
+    }
+  }
+}
