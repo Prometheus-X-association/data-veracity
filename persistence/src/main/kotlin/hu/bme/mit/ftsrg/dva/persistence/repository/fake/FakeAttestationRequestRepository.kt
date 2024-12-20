f991ed189723507dc09334fc67268253dfc52858
bp99 <bertalan.peter@bp99.eu>
2024-12-20 11:32:57 +0100

    fix: use AttestationRequest DTO instead of BO

--- persistence/src/main/kotlin/hu/bme/mit/ftsrg/dva/persistence/repository/fake/FakeAttestationRequestRepository.kt
+++ persistence/src/main/kotlin/hu/bme/mit/ftsrg/dva/persistence/repository/fake/FakeAttestationRequestRepository.kt
@@ -1,22 +1,22 @@
 package hu.bme.mit.ftsrg.dva.persistence.repository.fake
 
-import hu.bme.mit.ftsrg.dva.dto.AttestationRequest
+import hu.bme.mit.ftsrg.dva.dto.aov.AttestationRequestDTO
 import hu.bme.mit.ftsrg.dva.persistence.repository.AttestationRequestRepository
 import java.util.*
 
 class FakeAttestationRequestRepository : AttestationRequestRepository {
-  private val queue: Queue<AttestationRequest> = LinkedList()
+  private val queue: Queue<AttestationRequestDTO> = LinkedList()
 
-  override fun all(): List<AttestationRequest> = queue.toList()
+  override fun all(): List<AttestationRequestDTO> = queue.toList()
 
-  override fun byID(id: String): AttestationRequest? = queue.find { it.id == id }
+  override fun byID(id: String): AttestationRequestDTO? = queue.find { it.id == id }
 
-  override fun enqueue(entity: AttestationRequest): String {
+  override fun enqueue(entity: AttestationRequestDTO): String {
     val id = UUID.randomUUID().toString()
-    val requestWithID: AttestationRequest = entity.copy(id = id)
+    val requestWithID: AttestationRequestDTO = entity.copy(id = id)
     queue.add(requestWithID)
     return id
   }
 
-  override fun dequeue(): AttestationRequest? = queue.poll()
+  override fun dequeue(): AttestationRequestDTO? = queue.poll()
 }
