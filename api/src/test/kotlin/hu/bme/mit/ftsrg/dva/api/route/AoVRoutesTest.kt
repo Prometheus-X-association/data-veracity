eb53ca786f39d1c391e61b31fe4e2e49a5f03af2
bp99 <bertalan.peter@bp99.eu>
2025-01-20 22:24:24 +0100

    test: use testcontainers where possible and needed

--- api/src/test/kotlin/hu/bme/mit/ftsrg/dva/api/route/AoVRoutesTest.kt
+++ api/src/test/kotlin/hu/bme/mit/ftsrg/dva/api/route/AoVRoutesTest.kt
@@ -1,6 +1,6 @@
 package hu.bme.mit.ftsrg.dva.api.route
 
-import com.github.fridujo.rabbitmq.mock.MockConnectionFactory
+import com.rabbitmq.client.ConnectionFactory
 import hu.bme.mit.ftsrg.contractmanager.contract.model.Contract
 import hu.bme.mit.ftsrg.contractmanager.contract.model.Negotiator
 import hu.bme.mit.ftsrg.contractmanager.contract.model.Status
@@ -20,10 +20,17 @@ import io.ktor.server.testing.*
 import org.apache.jena.iri.IRIFactory
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.testcontainers.containers.RabbitMQContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import java.net.URI
 
 /* TODO: These tests should not be coupled with FakeAttestationRequestRepository */
+@Testcontainers
 class AoVRoutesTest {
+
+  @Container
+  val rmqContainer: RabbitMQContainer = RabbitMQContainer("rabbitmq").withExposedPorts(5672)
 
   @Test
   fun `should create attestation request`() = testApplication {
@@ -65,17 +72,20 @@ class AoVRoutesTest {
       Assertions.assertEquals(HttpStatusCode.OK, status)
     }
   }
+
+  private fun ApplicationTestBuilder.setupApplication() = application {
+    val rmqConnectionFactory = ConnectionFactory().apply {
+      host = rmqContainer.host
+      port = rmqContainer.firstMappedPort
+    }
+
+    testModule()
+    aovRoutes(rmqConnection = rmqConnectionFactory.newConnection())
+  }
+
+  private fun ApplicationTestBuilder.setupClient(): HttpClient = createClient {
+    install(ContentNegotiation) { json() }
+  }
+
+  private val iriFactory = IRIFactory.iriImplementation()
 }
-
-private fun ApplicationTestBuilder.setupApplication() = application {
-  val mockFactory = MockConnectionFactory()
-
-  testModule()
-  aovRoutes(rmqConnection = mockFactory.newConnection())
-}
-
-private fun ApplicationTestBuilder.setupClient(): HttpClient = createClient {
-  install(ContentNegotiation) { json() }
-}
-
-private val iriFactory = IRIFactory.iriImplementation()
