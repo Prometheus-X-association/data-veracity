1666ee0d4fb31547116ae0e64e195972eab6323f
hrajmund <hubairajmund0507@gmail.com>
2025-04-26 15:56:55 +0200

    feat(ssi): add ACA-Py integration
    Currently it handles two components: Data Consumer and Data Provider.
    Modified compose.yml by adding two aca-py agents and their controllers,
    one is for the Data Consumer called dva-consumer-side-aca-py and the other
    one is for the Data Provider called dva-provider-side-aca-py

---
+++ dva-python/src/dva-aca-py/aov.py
@@ -0,0 +1,21 @@
+import uuid
+from datetime import datetime
+from typing import List, Dict, Any
+from pydantic import BaseModel
+
+
+class Evaluation(BaseModel):
+    eval: List[Dict[str, Any]] = []
+    raw_json: str = None
+
+"""
+https://docs.pydantic.dev/latest/api/base_model/
+"""
+class AOV(BaseModel):
+    vc_id: str
+    valid_since: datetime
+    subject: str
+    issuer_id: str
+    record_id: str
+    contract_id: str
+    evaluations: Evaluation
---
+++ dva-python/src/dva-aca-py/provider.Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+COPY static/ ./static/
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD ["uvicorn", "aca_py_controller:app", "--host", "0.0.0.0", "--port", "8051", "--reload"]
