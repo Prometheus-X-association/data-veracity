5517396d3af9bad8aca05e745ee06cc3d1af4321
bp99 <bertalan.peter@bp99.eu>
2025-04-28 18:53:23 +0200

    test: add example unit test to dva-python

---
+++ dva-python/tests/test_example.py
@@ -0,0 +1,10 @@
+def square(x):
+    return x * x
+
+def test_square_int():
+    assert square(2) == 4
+    assert square(5) == 25
+    assert square(10) == 100
+
+def test_square_float():
+    assert square(1.5) == 2.25
