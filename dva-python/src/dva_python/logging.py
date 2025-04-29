ecfe99cf131aca45ff406fc657a96b9bdc92a36e
bzp99 <bpeter@edu.bme.hu>
2025-04-29 12:29:50 +0200

    refactor(proc): move logging to separate file

---
+++ dva-python/src/dva_python/logging.py
@@ -0,0 +1,30 @@
+from sys import stderr
+
+import structlog
+from structlog import make_filtering_bound_logger
+from structlog.dev import ConsoleRenderer
+from structlog.processors import TimeStamper, StackInfoRenderer, JSONRenderer
+from structlog.stdlib import add_log_level
+
+import dva_python.config
+
+
+def setup_logging():
+    shared_processors = [
+        add_log_level,
+        StackInfoRenderer(),
+        TimeStamper(fmt='iso'),
+    ]
+
+    if stderr.isatty():
+        processors = shared_processors + [ConsoleRenderer()]
+    else:
+        processors = shared_processors + [JSONRenderer()]
+
+    structlog.configure(
+        processors=processors,
+        context_class=dict,
+        wrapper_class=make_filtering_bound_logger(
+            dva_python.config.LOG_LEVEL
+        ),
+    )
