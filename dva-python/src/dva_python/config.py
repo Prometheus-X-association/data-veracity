6fdb8d359f5eac5a51eafae6ddbe6d873f64e9c8
bp99 <bertalan.peter@bp99.eu>
2025-06-15 16:28:13 +0200

    refactor(proc): clean up MongoDB integration

--- dva-python/src/dva_python/config.py
+++ dva-python/src/dva_python/config.py
@@ -16,6 +16,11 @@ QUEUE_NAME = "ATTESTATION_REQUESTS"
 # RabbitMQ server hostname
 RABBITMQ_HOST = env.get("DVA_RABBITMQ_HOST", default="localhost")
 
+# MongoDB data
+MONGO_URL = env.get("DVA_MONGODB_URL", default="mongodb://localhost:27017")
+MONGO_DB = env.get("DVA_MONGODB_DB", default="dva")
+MONGO_COLLECTION = env.get("DVA_MONGODB_COLLECTION_REQUESTS", default="requests")
+
 # Log level (must be supported by structlog)
 LOG_LEVEL = env.get("DVA_LOG_LEVEL", default="info")
 
