786c0c6f597b8f45c542ecb2b0d988ff9c9a0de7
bp99 <bertalan.peter@bp99.eu>
2025-06-15 17:05:58 +0200

    feat(ssi): implement logging into MongoDB

--- dva-acapy-controller/src/dva_acapy_controller/config.py
+++ dva-acapy-controller/src/dva_acapy_controller/config.py
@@ -10,4 +10,8 @@ PEER_CONTROLLER_PORT_IN = env.get("PEER_CONTROLLER_PORT_IN")
 PEER_CONTROLLER_PORT_OUT = env.get("PEER_CONTROLLER_PORT_OUT")
 PEER_LABEL = env.get("PEER_LABEL")
 
+MONGO_URL = env.get("DVA_MONGODB_URL", default="mongodb://localhost:27017")
+MONGO_DB = env.get("DVA_MONGODB_DB", default="dva")
+MONGO_COLLECTION = env.get("DVA_MONGODB_COLLECTION_REQUESTS", default="requests")
+
 LOG_FILE = "log.json"
