6168037f36aff3a16cdae7339640f1eaeb2c728d
bzp99 <bpeter@edu.bme.hu>
2025-06-15 16:28:13 +0200

    refactor(proc): clean up MongoDB integration

--- dva-python/src/dva_python/processing.py
+++ dva-python/src/dva_python/processing.py
@@ -1,9 +1,12 @@
 import json
 
+from datetime import datetime
 from pydantic import BaseModel, PositiveInt
-from pymongo import MongoClient, PyMongoError
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 from typing import Any
 
+from .config import MONGO_URL, MONGO_DB, MONGO_COLLECTION
 from .log import get_logger
 from .qc import validate_data
 
@@ -12,6 +15,7 @@ logger = get_logger()
 
 
 class AoVRequest(BaseModel):
+    id: str
     contract: dict[str, Any]
     data: list[PositiveInt]
     attesterID: str
@@ -20,6 +24,7 @@ class AoVRequest(BaseModel):
 
 
 class AoVGenerationRequest(BaseModel):
+    request_id: str
     subject: str
     issuer_id: str
     payload: dict[str, Any]
@@ -30,10 +35,6 @@ def handle_aov_request(req: AoVRequest) -> AoVGenerationRequest:
     data_bytes = bytearray(req.data)
     data_string = data_bytes.decode(encoding="utf-8")
     logger.debug(f"Data in request: {data_string}", request_data=data_string)
-
-    mongo_client = MongoClient("mongodb://mongo:27017")
-    db = mongo_client["dva"]
-    collection = db["requests"]
 
     mapping = req.mapping
 
@@ -48,13 +49,23 @@ def handle_aov_request(req: AoVRequest) -> AoVGenerationRequest:
             logger.warning("Failed validation", results=results_dict)
 
         try:
-            collection.update_one(
-                {"id": req.id}, {"$set": {"successful": results["success"]}}
+            req_coll = MongoClient(MONGO_URL)[MONGO_DB][MONGO_COLLECTION]
+            req_coll.update_one(
+                {"requestID": req.id},
+                {
+                    "$set": {
+                        "evaluationPassing": results["success"],
+                        "evaluationDate": datetime.utcnow(),
+                        "evaluationResults": json.dumps(results_dict),
+                    }
+                },
             )
-        except PyMongoError as mongo_err:
-            logger.error(f"error: {mongo_err} when tried to save success")
+            logger.info(f"Successfully updated MongoDB entry for request {req.id}")
+        except PyMongoError as e:
+            logger.error(f"Failed to update MongoDB entry due to {e}", error=e)
 
         return AoVGenerationRequest(
+            request_id=req.id,
             subject=contract["dataProvider"],
             issuer_id=req.attesterID,
             payload={
