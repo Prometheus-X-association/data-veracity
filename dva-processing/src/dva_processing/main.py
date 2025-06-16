ee4f2bb3f015bd86fb48a4bab1619ebf947f75e5
bp99 <bertalan.peter@bp99.eu>
2025-06-16 23:39:58 +0200

    refactor(proc): rename dva-python to dva-processing

--- dva-processing/src/dva_processing/main.py
+++ dva-processing/src/dva_processing/main.py
@@ -1,10 +1,10 @@
 import plac
 import uvicorn
 
-import dva_python.config
-import dva_python.log
-import dva_python.msgconsumer
-import dva_python.dryrun
+import dva_processing.config
+import dva_processing.log
+import dva_processing.msgconsumer
+import dva_processing.dryrun
 
 
 @plac.flg(
@@ -13,20 +13,20 @@ import dva_python.dryrun
     abbrev="n",
 )
 def main(dry_run=False):
-    dva_python.config.cfg.dry_run = dry_run
-    dva_python.log.setup_logging()
+    dva_processing.config.cfg.dry_run = dry_run
+    dva_processing.log.setup_logging()
 
     if dry_run:
-        logger = dva_python.log.get_logger()
+        logger = dva_processing.log.get_logger()
         logger.warning(
             "Dry run mode! Running uvicorn/FastAPI w/o actual processing effects"
         )
 
         uvicorn.run(
-            dva_python.dryrun.app, host="127.0.0.1", port=5000, log_level="info"
+            dva_processing.dryrun.app, host="127.0.0.1", port=5000, log_level="info"
         )
     else:
-        dva_python.msgconsumer.run()
+        dva_processing.msgconsumer.run()
 
 
 if __name__ == "__main__":
