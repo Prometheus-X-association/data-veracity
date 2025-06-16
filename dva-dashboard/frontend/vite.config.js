6aba58366441b4d2a99c81bc008b448e3b308509
bzp99 <bpeter@edu.bme.hu>
2025-06-16 13:41:04 +0200

    feat(dash): improve interface

--- dva-dashboard/frontend/vite.config.js
+++ dva-dashboard/frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const BACKEND_URL = process.env.DVA_DASHBOARD_BACKEND_URL || 'http://localhost:3000'
+
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: BACKEND_URL,
         changeOrigin: true,
         secure: false
       }
