245dc9ad0684a0d943cf54ccc863f2c9074ecab5
bzp99 <bpeter@edu.bme.hu>
2025-06-18 04:36:46 +0200

    feat(man): add basic VLA manager backend

---
+++ vla-manager/backend/server.js
@@ -0,0 +1,32 @@
+import cors from 'cors'
+import express from 'express'
+
+import { parseArgs } from 'node:util'
+
+import { addRoutes as addMockRoutes } from './mock.js'
+import { addRoutes as addMongoRoutes } from './real.js'
+
+main().catch(err => console.log(err))
+
+async function main () {
+  const { values: { mock } } = parseArgs({
+    options: {
+      mock: {
+        type: 'boolean',
+        short: 'm'
+      }
+    }
+  })
+
+  const app = express()
+  app.use(cors())
+  app.use(express.json())
+
+  if (mock) {
+    await addMockRoutes(app)
+  } else {
+    await addMongoRoutes(app)
+  }
+
+  app.listen(3000, () => console.log('Listening on :3000'))
+}
