495c59807790693ebae5f36ae79c95691449d2c2
bzp99 <bpeter@edu.bme.hu>
2025-08-25 13:10:00 +0200

    fix(api): reenable commented code for RMQ

--- dva-api/api/src/main/kotlin/hu/bme/mit/ftsrg/dva/api/Application.kt
+++ dva-api/api/src/main/kotlin/hu/bme/mit/ftsrg/dva/api/Application.kt
@@ -1,7 +1,10 @@
 package hu.bme.mit.ftsrg.dva.api
 
+import com.rabbitmq.client.Connection
+import com.rabbitmq.client.ConnectionFactory
 import hu.bme.mit.ftsrg.dva.api.db.*
 import hu.bme.mit.ftsrg.dva.api.err.addHandlers
+import hu.bme.mit.ftsrg.dva.api.rabbit.connectWithRetry
 import hu.bme.mit.ftsrg.dva.api.route.*
 import hu.bme.mit.ftsrg.dva.log.ReqestLogRepo
 import hu.bme.mit.ftsrg.dva.log.VerifRequestLogRepo
@@ -61,12 +64,12 @@ fun Application.configureKoin() {
     val rabbitHost = environment.config.property("rabbitmq.host").getString()
 
     val appModule = module {
-        //single<Connection> {
-        //    ConnectionFactory().run {
-        //        host = rabbitHost
-        //        connectWithRetry(logger = log)
-        //    }
-        //}
+        single<Connection> {
+            ConnectionFactory().run {
+                host = rabbitHost
+                connectWithRetry(logger = log)
+            }
+        }
         single<HttpClient> {
             HttpClient(CIO) {
                 install(ClientContentNegotiation) {
