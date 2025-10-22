257948445d9d93641cebed0fe58fd60269002702
CserDani <cserhegyidaniel@gmail.com>
2025-10-22 19:24:52 +0200

    feat(api): add cards to test vlas

--- vla-manager/frontend/src/components/ListView.vue
+++ vla-manager/frontend/src/components/ListView.vue
@@ -1,11 +1,29 @@
 <template>
+  <SampleModal v-model="data" ref="sampleModal" @update:modelValue="onDataSelected"/>
   <div class="content">
     <header>
       <h2>View VLAs</h2>
       <RouterLink to="/create"><button>Create new VLA</button></RouterLink>
     </header>
     <div class="main-part">
-      <vue-json-pretty :data="vlas" :deep="1" />
+      <div v-for="vla in vlas">
+        <n-card :title="vla.id">
+          <p>Description: {{ vla.description }}</p>
+          <p>Engines:</p>
+          <ul>
+            <div v-for="engine in new Set(vla.quality.map(q => q.engine))">
+              <li>{{ engine }}</li>
+            </div>
+          </ul>
+          <template #action>
+            <button
+              @click="showModalAndSetID(vla.id)"
+              class="add-button">
+              Try 
+            </button>
+          </template>
+        </n-card>
+      </div>
     </div>
   </div>
 </template>
@@ -15,8 +33,45 @@
   import axios from 'axios'
   import VueJsonPretty from 'vue-json-pretty'
   import 'vue-json-pretty/lib/styles.css'
+  import { NCard } from 'naive-ui'
+  import SampleModal from './SampleModal.vue'
 
   const vlas = ref([])
+
+  const sampleModal = ref(null)
+
+  const showModalAndSetID = (id) => {
+    sampleModal.value?.show()
+    vlaID.value = id
+  }
+
+  const data = ref(null)
+
+  const vlaID = ref(null)
+
+  const onDataSelected = async (newData) => {
+    const body = {
+      "exchangeID": "xchg-0001",
+      "attesterID": "attester-0000",
+      "data": newData,
+      "contract": {
+        "id": "contract-0001",
+        "vla": {
+          "id": vlaID.value
+        }
+      }
+    }
+
+    try {
+      const response = await axios.post('/api/attestation', body)
+      console.log({
+        "status": response.status,
+        "data": response.data
+      })
+    } catch (err) {}
+
+    vlaID.value = null
+  }
 
   onMounted(async () => {
     const response = await axios.get('/api/vla')
@@ -46,5 +101,6 @@
   .main-part {
     flex-grow: 1;
     display: flex;
+    gap: 1rem;
   }
 </style>
